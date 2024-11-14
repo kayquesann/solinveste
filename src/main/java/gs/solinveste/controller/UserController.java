84c28b9f31076672676500ceeaa10c10b3e9ef6b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-14 20:24:32 -0300

    Creation of UserController

---
+++ src/main/java/gs/solinveste/controller/UserController.java
@@ -0,0 +1,57 @@
+package gs.solinveste.controller;
+
+import gs.solinveste.dto.ReadUserDTO;
+import gs.solinveste.dto.SaveUserDTO;
+import gs.solinveste.model.User;
+import gs.solinveste.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<SaveUserDTO> saveUser (@RequestBody @Valid SaveUserDTO userDTO) {
+        SaveUserDTO newUser = userService.saveUser(userDTO);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("email/{id}/{email}")
+    public ResponseEntity<ReadUserDTO> updateEmail (@PathVariable Integer id, @PathVariable String email) {
+        ReadUserDTO user = userService.updateEmail(id, email);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("email/{id}/{password}")
+    public ResponseEntity<String> updatePassword (@PathVariable Integer id, @PathVariable String password) {
+        String status = userService.updatePassword(id, password);
+        return ResponseEntity.ok(status);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ReadUserDTO> readUser (@PathVariable Integer id) {
+        ReadUserDTO user = userService.readUser(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReadUserDTO>> readUsers () {
+        List<ReadUserDTO> users = userService.readUsers();
+        return ResponseEntity.ok(users);
+    }
+
+}
